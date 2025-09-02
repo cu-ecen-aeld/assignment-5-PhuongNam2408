ca9ec3e70979d049ff3966374e4c6433e5c9aa4c
PhuongNam2408 <phuongnamnguyen240@gmail.com>
2025-09-03 00:40:29 +0700

    Assignment 5 part 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 8020d956856ca45d255e1c3ba58995233b14d3f0
+AESD_ASSIGNMENTS_VERSION = 01cd3af52c6f93b36149fcd428371616a0a4206c
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -33,6 +33,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/clean.sh $(TARGET_DIR)/bin
+	$(INSTALL) -m 0775 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -T $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
